645648c14d8f8a434b5f68aeb0472af40b25cfd1
portalplayer <portalplayer@crime.su>
2024-08-21 14:41:31 -0400

    BuiltInArguments: Added missing arguments

--- built-in-arguments.js
+++ built-in-arguments.js
@@ -2,5 +2,9 @@
 
 module.exports.logLevel = "--log-level"
 module.exports.logLevelShort = "-ll"
+module.exports.disableAnsiColoring = "--disable-ansi-coloring"
+module.exports.disableAnsiColoringShort = "-dac"
+module.exports.dontLogHeader = "--dont-log-header"
+module.exports.dontLogHeaderShort = "-dlh"
 module.exports.disableLogFile = "--disable-log-file"
 module.exports.disableLogFileShort = "-dlf"
