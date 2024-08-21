bcee363da40068c930d5aab4c249b4927d4d7b72
portalplayer <portalplayer@crime.su>
2024-08-21 14:39:21 -0400

    Project: Added built-in arguments

---
+++ built-in-arguments.js
@@ -0,0 +1,6 @@
+// Purpose: Hard coded argument strings
+
+module.exports.logLevel = "--log-level"
+module.exports.logLevelShort = "-ll"
+module.exports.disableLogFile = "--disable-log-file"
+module.exports.disableLogFileShort = "-dlf"
