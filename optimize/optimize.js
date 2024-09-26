e0515b82f7b31416bd4bfac6b479707f29643b61
portalplayer <portalplayer@crime.su>
2024-09-26 12:42:24 -0400

    Project: Added a way to test speeds

---
+++ optimize/optimize.js
@@ -0,0 +1,24 @@
+// Purpose: Internal optimize function
+
+const {performance} = require("perf_hooks")
+const {inspect} = require("util");
+
+// NOTE(kratcy): Yes, I required test here. Wanna fight about it?
+module.exports.test = require("../tests/test")
+
+module.exports.checkSpeed = (name, that, passedFunction, ...passedArguments) => {
+    const now = performance.now()
+
+    passedFunction.call(that, ...passedArguments)
+    
+    const finalTime = performance.now() - now
+    
+    {
+        let stringedArguments = ""
+
+        for (const passedArgument of passedArguments)
+            stringedArguments += `${inspect(passedArgument)}, `
+
+        console.log(`${name}(${stringedArguments.substring(0, stringedArguments.length - 2)}) took ~${finalTime / 1000} seconds (${finalTime}ms)`)
+    }
+}
