13b5d3b0bc967cf6bb863555732872bfddaeab07
portalplayer <portalplayer@crime.su>
2024-09-26 11:41:17 -0400

    Test: Added `logger` test

---
+++ tests/logger.js
@@ -0,0 +1,10 @@
+// Purpose: Logger test
+
+const Logger = require("../logger")
+
+Logger.trace("Hello, World")
+Logger.debug("Hello, World")
+Logger.info("Hello, World")
+Logger.warn("Hello, World")
+Logger.error("Hello, World")
+Logger.fatal("Hello, World")
