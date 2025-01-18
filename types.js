d095a5756fd379181e0b9c290fb0d8ce54071513
kratcy <portalplayer@crime.su>
2025-01-18 01:28:16 -0500

    Project: Added types helper

---
+++ types.js
@@ -0,0 +1,12 @@
+// Purpose: Helper typedefs
+
+/**
+ * @typedef {{[key: TKey]: TValue}} Dictionary<TKey, TValue>
+ * @template TKey
+ * @template TValue
+ */
+
+/**
+ * @typedef {?T} Optional<T>
+ * @template T
+ */
